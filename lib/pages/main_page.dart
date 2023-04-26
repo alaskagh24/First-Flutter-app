324cbf4f3c925618c37dc5781f90dd6512ac33a0
unknown <alaska0546122456@gmail.com>
2023-04-26 15:34:01 +0000

    My first Commit

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).accentColor,
+        title: Text(
+          "Main Page",
+          style: Theme.of(context).textTheme.headline1,
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
